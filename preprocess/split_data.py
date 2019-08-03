4a9698ffe8dc75819ed6e62fe0f9b5f2d88a8c00
Xiaoqi Lu <lx2170@cumc.columbia.edu>
2019-08-03 15:54:05 -0700

    get split data set
    usage: get_split_data()

--- preprocess/split_data.py
+++ preprocess/split_data.py
@@ -1,9 +1,35 @@
 import json
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 INPUT_FILE = '~/Data/arxiv_subject_classification/train.json'
-DATA = json.load(open(INPUT_FILE))
+# INPUT_FILE = '../data/train.json'
 
 
-def split(data=DATA):
-    
+def read_data(file=INPUT_FILE):
+    with open(file) as f:
+        df = pd.DataFrame(json.load(f))
+    return df
+
+
+def filter_data(df):
+    # remove duplicated titles
+    df = df[~df.title.duplicated()]
+    return df
+
+
+def split_data(df):
+    train, test = train_test_split(df, test_size=0.10, random_state=0, stratify=None)
+    return train, test
+
+
+def get_split_data(file=INPUT_FILE):
+    df = filter_data(read_data(file))
+    train, test = split_data(df)
+    return train, test
+
+
+if __name__ == '__main__':
+    df_train, df_test = get_split_data()
+    print(df_train.info())
+    print(df_test.info())
