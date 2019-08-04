23a6a5161cf2f4cd982ad79f3c81a51968b7e314
Jingjing Chen <chenjj235@gmail.com>
2019-08-03 18:25:52 -0700

    Update classification.py

--- train/classification.py
+++ train/classification.py
@@ -1,11 +1,15 @@
 import sys
 sys.path.append("../")
 
+import numpy as np
 from collections import defaultdict
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import accuracy_score
 
 from preprocess.split_data import get_split_data, split_data
+
+PATH = '/Users/jingjing/Data/arxiv_subject_classification/'
 
 
 def combine_corpus(df_train):
@@ -23,30 +27,70 @@ def combine_corpus(df_train):
 
 
 def tfidf(corpus_list, top_ngram=1):
-    vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word',
-                                 stop_words='english', token_pattern=r'\b[a-zA-Z]+\b',
-                                 ngram_range=(1, top_ngram), max_df=0.9, max_features=3000)
+    vectorizer = TfidfVectorizer(
+        strip_accents='unicode', analyzer='word',
+        stop_words='english', token_pattern=r'\b[a-zA-Z]+\b',
+        ngram_range=(1, top_ngram), max_df=0.9, max_features=3000)
     x = vectorizer.fit_transform(corpus_list)
-    return x, vectorizer.get_feature_names()
+    return x, vectorizer
 
 
-def transform_xy(df,
-                 subject_title_tfidf, title_words, subject_abstract_tfidf, abstract_words):
-    
+def transform_xy(df, title_matrix, title_vectorizer,
+                 abstract_matrix, abstract_vectorizer, subjects):
 
-    return X, y
+    df_len = len(df)
+    sub_len = len(subjects)
 
-def main():
+    X, y = [], []
+    for i, row in df.iterrows():
+        tvec, avec = title_vectorizer.transform([row['title']]), \
+                     abstract_vectorizer.transform([row['abstract']])
+
+        Xi = np.hstack([np.dot(title_matrix, tvec.T).T.toarray(),
+                        np.dot(abstract_matrix, avec.T).T.toarray()])
+        yi = subjects.index(row['subject'])
+        X.append(Xi); y.append(yi)
+    return np.array(X).reshape(df_len, sub_len*2), np.array(y)
+
+
+def save_xy(path=PATH):
+    print("Read Data")
     df_train, df_test = get_split_data()
 
     # combine texts in each subjects and find tfidf
+    print("Combine Corpus")
     subjects, subject_titles, subject_abstracts = combine_corpus(df_train)
-    subject_title_tfidf, title_words = tfidf([subject_titles[sub] for sub in subjects], top_ngram=2)
-    subject_abstract_tfidf, abstract_words = tfidf([subject_abstracts[sub] for sub in subjects])
+    title_matrix, title_vectorizer = tfidf([subject_titles[sub] for sub in subjects], top_ngram=2)
+    abstract_matrix, abstract_vectorizer = tfidf([subject_abstracts[sub] for sub in subjects])
 
     # train classification
-    df_train_lr, df_test_lr = split_data(df_test)
-    X_train, y_train = transform_xy(df_train_lr, subject_title_tfidf, title_words,
-                                    subject_abstract_tfidf, abstract_words)
-    X_test, y_test = transform_xy(df_test_lr, subject_title_tfidf, title_words,
-                                  subject_abstract_tfidf, abstract_words)
+    print("Transform XY")
+    #df_train_lr, df_test_lr = split_data(df_test)
+    df_train_lr, df_test_lr = df_train, df_test
+    X_train, y_train = transform_xy(df_train_lr, title_matrix, title_vectorizer,
+                                    abstract_matrix, abstract_vectorizer, subjects)
+    X_test, y_test = transform_xy(df_test_lr, title_matrix, title_vectorizer,
+                                  abstract_matrix, abstract_vectorizer, subjects)
+
+    print("Save to File")
+    np.save(path+'X_train.npy', X_train)
+    np.save(path + 'y_train.npy', y_train)
+    np.save(path + 'X_test.npy', X_test)
+    np.save(path + 'y_test.npy', y_test)
+
+
+def main(X_train, y_train, X_test, y_test):
+    print("Train")
+    LRCV = LogisticRegressionCV()
+    LRCV.fit(X_train, y_train)
+    y_pred = LRCV.predict(X_test)
+    print(f"Accuracy {accuracy_score(y_test, y_pred)}")
+
+
+if __name__ == '__main__':
+    save_xy()
+
+    main(np.load(PATH+'X_train.npy'),
+         np.load(PATH+'y_train.npy'),
+         np.load(PATH+'X_test.npy'),
+         np.load(PATH+'y_test.npy'))
